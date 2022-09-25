67403b520b5f8f59e86020e4f064cdb625522579
Jahir <engrmmjahir@gmail.com>
2022-09-26 04:37:06 +0600

    added cart section and saved local storeage

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css';
+
+const Cart = (props) => {
+    const { cart } = props;
+    let totalPrice = 0;
+    let totalShipping = 0;
+    let quantity = 0;
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = parseFloat((totalPrice * 0.1).toFixed(2));
+    const grandTotal = totalPrice + totalShipping + tax;
+    return (
+        <div className='cart'>
+            <h3>Order Summary</h3>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
+        </div >
+    );
+};
+
+export default Cart;
